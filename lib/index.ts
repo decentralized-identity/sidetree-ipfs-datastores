c8b73621108ebc8f6bc9a794b18676f0b9939f63
Anmol Agarwala <anmaga@microsoft.com>
2019-04-25 03:31:46 +0530

    added mock lock

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,10 @@
 import AzureDataStore, { AzureDSInputOptions } from './azure/AzureDataStore';
 import AzureBlobLock from './azure/AzureBlobLock';
+import MockLock from './azure/MockLock';
 
 export {
   AzureBlobLock,
   AzureDataStore,
-  AzureDSInputOptions
+  AzureDSInputOptions,
+  MockLock
 };
