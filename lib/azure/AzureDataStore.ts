069a22d2c148c01338c091641176f125945e22fc
Isaac Chen <ische@microsoft.com>
2020-05-14 12:42:38 -0700

    update interface and respective implementation

--- lib/azure/AzureDataStore.ts
+++ lib/azure/AzureDataStore.ts
@@ -1,18 +1,7 @@
 import * as storage from 'azure-storage';
+import * as path from 'upath';
 import WritableMemoryStream from './WritableMemoryStream';
-const setImmediate = require('async/setImmediate');
-const each = require('async/each');
-const waterfall = require('async/series');
-const path = require('upath');
-const asyncFilter = require('interface-datastore').utils.asyncFilter;
-const asyncSort = require('interface-datastore').utils.asyncSort;
-
-const iDatastore = require('interface-datastore');
-const Key = iDatastore.Key;
-const Errors = iDatastore.Errors;
-
-const DEFERRED = require('pull-defer');
-const pull = require('pull-stream');
+import { Key, Errors, Adapter } from 'interface-datastore';
 
 /**
  * Structure for input params for Azure Data store
@@ -25,9 +14,9 @@ export type AzureDSInputOptions = {
 };
 
 /**
- * Azure data store class that implements IDataStore
+ * Azure data store class that implements interface-datastore's adapter
  */
-export default class AzureDataStore {
+export default class AzureDataStore extends Adapter {
   private path: string;
   private blobService: storage.BlobService;
   private container: string;
@@ -36,9 +25,9 @@ export default class AzureDataStore {
    * Constructor to initialize the class
    * @param path path to azure blob storage container
    * @param opts Azure DS input options
-   * @param containerName Azure blob storage container name
    */
   public constructor (path: string, opts: AzureDSInputOptions) {
+    super();
     this.path = path;
     this.container = opts.containerName;
     this.blobService = opts.blobService;
@@ -58,252 +47,164 @@ export default class AzureDataStore {
   }
   /**
    * Returns the full key which includes the path to the ipfs store
-   * @param key
+   * @param key The key to get the full path for
    */
   private getFullKey (key: any): string {
     return path.join('.', this.path, key.toString());
   }
 
   /**
-   * Recursively fetches all keys from azure blob storage
-   * @param params
-   * @param keys
-   * @param callback
+   * Fetches keys from storage
+   * @param prefix The prefix to filter for
+   * @param currentToken The current token to know where in the index to start
+   * @return an object containing continuation token indicating there are more keys to retrieve, and a list of keys
    */
-  private listKeys (prefix: string, currentToken: any, keys: any, callback: any): void {
-    if (typeof callback === 'undefined') {
-      callback = keys;
-      keys = [];
-    }
-
-    this.blobService.listBlobsSegmentedWithPrefix(this.container, prefix, currentToken, (err, result, response) => {
-      if (err) {
-        return callback(new Error(err.name));
-      }
-      if (response.isSuccessful) {
-        result.entries.forEach((d) => {
-          keys.push(new Key(d.name.slice(this.path.length), false));
-        });
-
-        if (result.continuationToken) {
-          return this.listKeys(prefix, result.continuationToken, keys, callback);
+  private async listKeys (prefix: string, currentToken: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.blobService.listBlobsSegmentedWithPrefix(this.container, prefix, currentToken, async (err, result, response) => {
+        if (err) {
+          reject(new Error(err.name));
+          return;
         }
-      }
+        if (response.isSuccessful) {
+          const keys = [];
+          result.entries.forEach((d) => {
+            keys.push(new Key(d.name.slice(this.path.length), false));
+          });
 
-      callback(err, keys);
+          resolve({
+            keys: keys,
+            continuationToken: result.continuationToken
+          });
+          return;
+        }
+      });
     });
   }
 
   /**
-   * Returns an iterator for fetching objects from azure by their key
-   * @param keys
-   * @param keysOnly Whether or not only keys should be returned
+   * returns an asyncIterator that yields all the keys and values in the store
    */
-  private getBlobIterator (keys: any, keysOnly: boolean): any {
-    let count: number = 0;
-
-    return {
-      next: (callback: any) => {
-        if (count >= keys.length) {
-          return callback(null, null, null);
-        }
-        let currentKey = keys[count++];
-        if (keysOnly) {
-          return callback(null, currentKey, null);
-        }
-        this.get(currentKey, (err: any, data: any) => {
-          callback(err, currentKey, data);
-        });
+  private async * _all () {
+    // log when this is being called so we know this is not happening too much
+    console.warn('_all is being called by ipfs, this is inefficient and should not happen often');
+    let continuationToken = null;
+    do {
+      const listKeysResult = await this.listKeys('', continuationToken);
+      const keys = listKeysResult.keys;
+      for (const key of keys) {
+        const value = await this.get(key);
+        yield { key: key, value: value };
       }
-    };
+      continuationToken = listKeysResult.continuationToken;
+    } while (continuationToken);
   }
 
   /**
    * Store the given value under the key.
-   * @param key
-   * @param val
-   * @param callback
+   * @param key The key to put
+   * @param val The value to put
    */
-  public put (key: any, val: Buffer, callback: any): void {
-    this.blobService.createBlockBlobFromText(this.container, this.getFullKey(key), val, (err, _result, _response) => {
-      if (err) {
-        return callback(Errors.dbWriteFailedError(err));
-      }
-      callback();
+  public async put (key: any, val: Buffer): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.blobService.createBlockBlobFromText(this.container, this.getFullKey(key), val, (err, _result, _response) => {
+        if (err) {
+          reject(Errors.dbWriteFailedError(err));
+          return;
+        }
+        resolve();
+        return;
+      });
     });
+
   }
 
   /**
    * Read content from azure blob storage.
-   * @param key
-   * @param callback
+   * @param key The key to get the value for
    */
-  public get (key: any, callback: any): void {
+  public async get (key: any): Promise<any> {
     let writeStream: WritableMemoryStream = new WritableMemoryStream();
+    return new Promise((resolve, reject) => {
+      writeStream.on('finish', () => {
+        resolve(writeStream.fetchData());
+        return;
+      });
 
-    writeStream.on('finish', () => {
-      callback(null, writeStream.fetchData());
-    });
-
-    this.blobService.getBlobToStream(this.container, this.getFullKey(key), writeStream, (err, _result, _response) => {
-      if (err && err.message === 'NotFound') {
-        return callback(Errors.notFoundError(err));
-      } else if (err) {
-        return callback(err);
-      }
+      this.blobService.getBlobToStream(this.container, this.getFullKey(key), writeStream, (err, _result, _response) => {
+        if (err && err.message === 'NotFound') {
+          reject(Errors.notFoundError(err));
+          return;
+        } else if (err) {
+          reject(err);
+          return;
+        }
+      });
     });
   }
 
   /**
    * Check for the existence of the given key.
-   * @param key
-   * @param callback
+   * @param key the key to check has on
    */
-  public has (key: any, callback: any): void {
-    this.blobService.doesBlobExist(this.container, this.getFullKey(key), (err, result, _response) => {
-      if (err) {
-        callback(err, false);
-      } else if (result && result.exists) {
-        callback(null, true);
-      } else {
-        callback(null, false);
-      }
+  public async has (key: any): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.blobService.doesBlobExist(this.container, this.getFullKey(key), (err, result, _response) => {
+        if (err) {
+          reject(err);
+          return;
+        } else if (result && result.exists) {
+          resolve(true);
+          return;
+        } else {
+          resolve(false);
+          return;
+        }
+      });
     });
   }
 
   /**
    * Delete the record under the given key.
-   * @param key
-   * @param callback
+   * @param key The key to delete
    */
-  public delete (key: any, callback: any): void {
-    this.blobService.deleteBlobIfExists(this.container, this.getFullKey(key), (err, _result, _response) => {
-      if (err) {
-        return callback(Errors.dbDeleteFailedError(err));
-      }
-      callback();
-    });
-  }
-
-  /**
-   * Creates a new batch object
-   */
-  public batch (): any {
-    let puts = [
-      {
-        key: '',
-        value: Buffer.from('')
-      }];
-    let deletes: any = [];
-    return {
-      put (key: any, value: any): void {
-        puts.push({ key: key, value: value });
-      },
-      delete (key: any): void {
-        deletes.push(key);
-      },
-      commit: (callback: any) => {
-        waterfall([
-          (cb: any) => each(puts, (p: any, _cb: any) => {
-            this.put(p.key, p.value, _cb);
-          }, cb),
-          (cb: any) => each(deletes, (key: any, _cb: any) => {
-            this.delete(key, _cb);
-          }, cb)
-        ], (err: any) => callback(err));
-      }
-    };
-  }
-
-  /**
-   * Query the azure blob storage
-   * @param q
-   */
-  public query (q: any): any {
-    const prefix = path.join(this.path, q.prefix || '');
-
-    let deferred = DEFERRED.source();
-    let iterator: any;
-
-    const rawStream = (end: any, callback: any) => {
-      if (end) {
-        return callback(end);
-      }
-      iterator.next((err: any, key: any, value: any) => {
+  public delete (key: any): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.blobService.deleteBlobIfExists(this.container, this.getFullKey(key), (err, _result, _response) => {
         if (err) {
-          return callback(err);
+          reject(Errors.dbDeleteFailedError(err));
+          return;
         }
-        if (err === null && key === null && value === null) {
-          return callback(true);
-        }
-
-        const res = {
-          key: key,
-          value: Buffer.from('')
-        };
-
-        if (value) {
-          res.value = value;
-        }
-        callback(null, res);
+        resolve();
+        return;
       });
-    };
-
-    this.listKeys(prefix, null, [], (err: any, keys: any) => {
-      if (err) {
-        return deferred.abort(err);
-      }
-
-      iterator = this.getBlobIterator(keys, q.keysOnly || false);
-      deferred.resolve(rawStream);
     });
-
-    // Use a deferred pull stream source, as async operations need to occur before the
-    // pull stream begins
-    let tasks = [deferred];
-
-    if (q.filters != null) {
-      tasks = tasks.concat(q.filters.map((f: any) => asyncFilter(f)));
-    }
-
-    if (q.orders != null) {
-      tasks = tasks.concat(q.orders.map((o: any) => asyncSort(o)));
-    }
-
-    if (q.offset != null) {
-      let i = 0;
-      tasks.push(pull.filter(() => i++ >= q.offset));
-    }
-
-    if (q.limit != null) {
-      tasks.push(pull.take(q.limit));
-    }
-
-    return pull.apply(null, tasks);
   }
 
   /**
    * This will check the blob storage for container's access and existence
-   * @param callback
    */
-  public open (callback: any): void {
-    this.blobService.doesBlobExist(this.container, this.path, (err, _result, response) => {
-      if (err) {
-        return callback(Errors.dbOpenFailedError(err));
-      }
-      if (response.statusCode === 404) {
-        return this.put(new Key('/', false), Buffer.from(''), callback);
-      }
+  public async open (): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.blobService.doesBlobExist(this.container, this.path, async (err, _result, response) => {
+        if (err) {
+          reject(Errors.dbOpenFailedError(err));
+          return;
+        }
+        if (response.statusCode === 404) {
+          await this.put(new Key('/', false), Buffer.from(''));
+        }
 
-      callback();
+        console.log('Azure data store opened.');
+        resolve();
+      });
     });
   }
 
   /**
    * Close the store.
-   * @param callback
    */
-  public close (callback: any): void {
-    setImmediate(callback);
+  public async close (): Promise<void> {
+    console.log('Azure data store closed.');
   }
 }
--- test/azure/AzureDataStore.spec.ts
+++ test/azure/AzureDataStore.spec.ts
@@ -29,32 +29,36 @@ describe('AzureDataStore', () => {
   });
 
   describe('put', () => {
-    it('should include the path in the key', (done) => {
+    it('should include the path in the key', async (done) => {
       standin.replace(blobStore.getBlobService(), 'createBlockBlobFromText', (stand, _name, key, _value, callback) => {
         expect(key).toEqual('.ipfs/datastore/z/key');
         stand.restore();
         callback(null);
+        done();
       });
 
-      blobStore.put(new Key('/z/key'), Buffer.from('test data'), done);
+      await blobStore.put(new Key('/z/key'), Buffer.from('test data'));
     });
 
-    it('should return a standard error when the put fails', (done) => {
+    it('should return a standard error when the put fails', async (done) => {
       standin.replace(blobStore.getBlobService(), 'createBlockBlobFromText', (stand, _name, key, _value, callback) => {
         expect(key).toEqual('.ipfs/datastore/z/key');
         stand.restore();
         callback(new Error('bad things happened'));
       });
 
-      blobStore.put(new Key('/z/key'), Buffer.from('test data'), (err) => {
+      try {
+        await blobStore.put(new Key('/z/key'), Buffer.from('test data'));
+        fail('expected to throw but did not');
+      } catch (err) {
         expect(err.code).toEqual('ERR_DB_WRITE_FAILED');
         done();
-      });
+      }
     });
   });
 
   describe('get', () => {
-    it('should include the path in the fetch key', (done) => {
+    it('should include the path in the fetch key', async (done) => {
       let writeStream = new WritableMemoryStream();
 
       standin.replace(blobStore.getBlobService(), 'getBlobToStream', (stand, _name, key, writeStream, callback) => {
@@ -64,10 +68,10 @@ describe('AzureDataStore', () => {
         done();
       });
 
-      blobStore.get(new Key('/z/key'), done);
+      await blobStore.get(new Key('/z/key'));
     });
 
-    it('should return a standard not found error code if the key isnt found', (done) => {
+    it('should return a standard not found error code if the key is not found', async (done) => {
       standin.replace(blobStore.getBlobService(), 'getBlobToStream', (stand, _name, key, _writeStream, callback) => {
         expect(key).toEqual('.ipfs/datastore/z/key');
         stand.restore();
@@ -75,39 +79,119 @@ describe('AzureDataStore', () => {
         callback(error, null, { statusCode: 404 });
       });
 
-      blobStore.get(new Key('/z/key'), (err) => {
+      try {
+        await blobStore.get(new Key('/z/key'));
+        fail('expected to throw but did not');
+      } catch (err) {
         expect(err.code).toEqual('ERR_NOT_FOUND');
         done();
-      });
+      }
     });
   });
 
   describe('delete', () => {
-    it('should return a standard delete error if deletion fails', (done) => {
+    it('should return a standard delete error if deletion fails', async (done) => {
       standin.replace(blobStore.getBlobService(), 'deleteBlobIfExists', (stand, _name, key, callback) => {
         expect(key).toEqual('.ipfs/datastore/z/key');
         stand.restore();
         callback(new Error('bad things'));
       });
 
-      blobStore.delete(new Key('/z/key'), (err) => {
+      try {
+        await blobStore.delete(new Key('/z/key'));
+        fail('expected to throw but did not');
+      } catch (err) {
         expect(err.code).toEqual('ERR_DB_DELETE_FAILED');
         done();
+      }
+    });
+  });
+
+  describe('has', () => {
+    it('should return true if found', async () => {
+      standin.replace(blobStore.getBlobService(), 'doesBlobExist', (stand, _name, _key, callback) => {
+        stand.restore();
+        callback(undefined, { exists: true });
       });
+
+      const result = await blobStore.has('something');
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false if not found', async () => {
+      standin.replace(blobStore.getBlobService(), 'doesBlobExist', (stand, _name, _key, callback) => {
+        stand.restore();
+        callback(undefined, { exists: false });
+      });
+
+      const result = await blobStore.has('something');
+      expect(result).toBeFalsy();
+    });
+
+    it('should return false if not result', async () => {
+      standin.replace(blobStore.getBlobService(), 'doesBlobExist', (stand, _name, _key, callback) => {
+        stand.restore();
+        callback(undefined, undefined);
+      });
+
+      const result = await blobStore.has('something');
+      expect(result).toBeFalsy();
+    });
+
+    it('should throw error on error', async () => {
+      standin.replace(blobStore.getBlobService(), 'doesBlobExist', (stand, _name, _key, callback) => {
+        stand.restore();
+        callback(new Error('error for testing'));
+      });
+
+      try {
+        const result = await blobStore.has('something');
+        fail('expect to throw but did not');
+      } catch (e) {
+        expect(e.message).toEqual('error for testing');
+      }
+    });
+  });
+
+  describe('_all', () => {
+    it('should return an async iterator over all keys and values', async () => {
+      standin.replace(blobStore.getBlobService(), 'listBlobsSegmentedWithPrefix', (stand, _container, _prefix, currentToken, callback) => {
+        if (currentToken === 'end') {
+          stand.restore();
+          callback(undefined, { entries: [{ name: '.ipfs/datastore/key2' }] }, { isSuccessful: true });
+        } else {
+          callback(undefined, { entries: [{ name: '.ipfs/datastore/key1' }], continuationToken: 'end' }, { isSuccessful: true });
+        }
+      });
+      spyOn(blobStore, 'get').and.callFake(() => {
+        return Buffer.from('value');
+      });
+
+      const results = blobStore['_all'](); // this is an iterator
+      const expected = [{ key: new Key('/key1'), value: Buffer.from('value') }, { key: new Key('/key2'), value: Buffer.from('value') }];
+      let indexCounter = 0;
+      // tslint:disable-next-line: await-promise
+      for await (const result of results) {
+        expect(result).toEqual(expected[indexCounter]);
+        indexCounter++;
+      }
     });
   });
 
   describe('open', () => {
-    it('should return a standard open error if blob exist check fails', (done) => {
+    it('should return a standard open error if blob exist check fails', async (done) => {
       standin.replace(blobStore.getBlobService(), 'doesBlobExist', (stand, _name, _key, callback) => {
         stand.restore();
         callback(new Error('unknown'));
       });
 
-      blobStore.open((err) => {
+      try {
+        await blobStore.open();
+        fail('expected to throw but did not');
+      } catch (err) {
         expect(err.code).toEqual('ERR_DB_OPEN_FAILED');
         done();
-      });
+      }
     });
   });
 });
